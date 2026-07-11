067180792b01b9be5837a73a375c403a92bc4d5f
Suzuha88 <didudun80@gmail.com>
2026-07-11 11:17:48 +0400

    Creating async engine and session generator

---
+++ src/delivery_service/core/db.py
@@ -0,0 +1,18 @@
+from typing import AsyncGenerator
+
+from config import settings
+from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
+
+URL = settings.DATABASE_URL
+
+async_engine = create_async_engine(
+    URL,
+    echo=True)
+
+AsyncSessionMaker = async_sessionmaker(
+    bind=async_engine, expire_on_commit=False)
+
+
+async def get_session() -> AsyncGenerator[AsyncSession, None]:
+    async with AsyncSessionMaker() as session:
+        yield session
