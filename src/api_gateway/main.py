a98e9f62fa373e44172adada8c30b8e7138074b9
Suzuha88 <didudun80@gmail.com>
2026-07-12 19:33:39 +0400

    Annotations, etc.

--- src/api_gateway/main.py
+++ src/api_gateway/main.py
@@ -1,22 +1,23 @@
 from contextlib import asynccontextmanager
 from json import dumps as json_dumps
 from secrets import token_hex
-from typing import Annotated, Any
+from typing import AsyncGenerator
 
 import uvicorn
 from aio_pika import Message
-from fastapi import FastAPI, Request, Response
+from fastapi import FastAPI, Request, Response, status
 from schemas.schemas import PackageSchema
 from shared.config import settings
 from shared.db import initialize_db
 from shared.rabbit import initialize_rabbitmq
+from starlette.middleware.base import RequestResponseEndpoint
 
 if __name__ == "__main__":
     uvicorn.run("main:app", reload=True, port=settings.API_GATEWAY_PORT)
 
 
 @asynccontextmanager
-async def lifespan(app: FastAPI):
+async def lifespan(app: FastAPI) -> AsyncGenerator[None, None]:
     await initialize_db()
     connection, channel, queue = await initialize_rabbitmq()
 
@@ -31,7 +32,10 @@ app = FastAPI(lifespan=lifespan)
 
 
 @app.middleware("http")
-async def add_session_id(request: Request, call_next) -> None | Response:
+async def add_session_id(
+    request: Request,
+    call_next: RequestResponseEndpoint
+) -> None | Response:
     session_id = request.cookies.get("session_id")
 
     if not session_id:
@@ -56,9 +60,10 @@ async def register(
         package: PackageSchema,
         request: Request,
 
-) -> Any:
+) -> Response:
 
-    session_id = request.cookies.get("session_id") or request.state.session_id
+    session_id = request.cookies.get(
+        "session_id") or request.state.session_id
 
     channel = app.state.channel
     dict_body = package.model_dump()
@@ -67,7 +72,14 @@ async def register(
 
     byte_body = json_dumps(dict_body).encode("utf-8")
 
-    await channel.default_exchange.publish(
-        Message(byte_body),
-        routing_key=app.state.queue.name
-    )
+    try:
+        await channel.default_exchange.publish(
+            Message(byte_body),
+            routing_key=app.state.queue.name
+        )
+        return Response(content={"message": "Package sent for registration"},
+                        status_code=status.HTTP_200_OK)
+    except Exception as e:
+        return Response(
+            content={"error": f"Couldn't send package for registration: {e}"},
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR)
